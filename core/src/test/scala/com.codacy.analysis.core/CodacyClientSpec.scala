91768888ebc3d159bca84a912ecd51838a7a340b
ads875646 <97050623+ads875646@users.noreply.github.com>
2022-01-03 20:11:00 +0100

    Update CodacyClientSpec.scala

--- core/src/test/scala/com.codacy.analysis.core/CodacyClientSpec.scala
+++ core/src/test/scala/com.codacy.analysis.core/CodacyClientSpec.scala
@@ -26,9 +26,6 @@ class CodacyClientSpec extends Specification with NoLanguageFeatures with Mockit
   private val commitUuid = Commit.Uuid("some_commitUuid")
   private val remoteUrl = "codacy.com/2.0"
   private val tool = "eslint"
-
-  private TypeInexistant Erreur "erreur" ( == [
-  
   
   private val apiCredentials: Credentials =
     APIToken(apiTokenStr, remoteUrl, provider, UserName(username), ProjectName(project))
